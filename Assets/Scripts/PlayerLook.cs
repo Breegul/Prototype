b314251970e470b6bfaa2a1119208940560b7947
Martin Torrado Mouzo <breegul0089@gmail.com>
2020-10-28 03:00:59 +0000

    Added player and movement

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float xSensitivity;
+    public float ySensitivity;
+
+    private float upRotation = 0;
+    private GameObject player;
+    
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked; // hide cursor
+    }
+
+    void Update()
+    {
+        // looks up but can't rotate too far
+        upRotation -= Input.GetAxis("Mouse Y") * ySensitivity * Time.deltaTime;
+        upRotation = Mathf.Clamp(upRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(upRotation, 0f, 0f);
+
+        // look around
+        float x = Input.GetAxis("Mouse X") * xSensitivity * Time.deltaTime;
+        player.transform.Rotate(Vector3.up*x);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+
+    private Vector3 velocity;
+    private float gravity = -10f;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    private CharacterController cc;
+
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //if player is grounded make velocity constant
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.3f, groundMask);
+        if(isGrounded&&velocity.y<0) velocity.y = -1f;
+
+        // (right vector*hInput + forward vector*vInput) * speed
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        cc.Move(move*speed*Time.deltaTime);
+
+        if(isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // gravity (multiply by time twice because maths)
+        velocity.y += gravity*Time.deltaTime;
+        cc.Move(velocity*Time.deltaTime);
+    }
+}
