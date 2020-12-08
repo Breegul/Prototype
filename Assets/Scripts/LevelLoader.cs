87807c53d4b5f21eb86e1e78d72e216ca9b9b644
Breegul <31014184+Breegul@users.noreply.github.com>
2020-12-08 03:14:51 +0000

    level loading start

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            LoadNext();
+        }
+    }
+
+    public void LoadNext()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(index);
+    }
+}
